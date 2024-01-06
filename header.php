da50a60f20fb861e382488d19a801f87a5cc9d9c
kaungsatpaing98 <kaungsatpaing@my.jcu.edu.au>
2024-01-06 20:30:57 +0800

    Testing Commit

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
     <nav>
         <ul>
             <li><a href="<?php echo esc_url(home_url('/')); ?>">Home</a></li>
-            
+            <li><a href="<?php echo esc_url(home_url('/')); ?>">Event</a></li>
             <li><a href="<?php echo esc_url(home_url('/highlight-page.php')); ?>">Highlights</a></li>
             <li><a href="<?php echo esc_url(home_url('/contact-page.php')); ?>">Contact</a></li>
             <li><a href="<?php echo esc_url(get_template_directory_uri() . '/contact-page.php'); ?>">Custom Page</a></li>
